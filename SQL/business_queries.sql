744fd0dc4183cad25ef2cc3319375794cf501ef4
poojachavan8225 <poojachavan8225@gmail.com>
2026-08-01 22:41:47 +0530

    bussines querries done

--- SQL/business_queries.sql
+++ SQL/business_queries.sql
@@ -49,6 +49,75 @@ group by category order by total_customers desc;
  from customer_purchasing_db.customer_purchases
  group by season
  order by total_revenue desc ;
+ -- Payment Method Analysis
+ SELECT
+    payment_method,
+    COUNT(*) AS total_transactions
+FROM customer_purchases
+GROUP BY payment_method
+ORDER BY total_transactions DESC;
+--  Customers by Age Group
+select age_group,
+ count(*) as total_customers 
+ from customer_purchasing_db.customer_purchases
+ group by age_group
+ order by total_customers desc;
  
+ -- Categories with Revenue > 50000
+ select category,
+ sum(purchase_amount) as total_revenue
+ from customer_purchasing_db.customer_purchases
+ group by category
+ having total_revenue >= 63000;
  
+  -- payment methods used by more than 500 customers
+  select payment_method,
+  count(*) as total_customers
+  from customer_purchasing_db.customer_purchases
+  group by payment_method
+  having total_customers > 500 
+  order by total_customers desc;
+ 
+ SELECT
+    customer_id,
+    purchase_amount,
+    CASE
+        WHEN purchase_amount >= 80 THEN 'High Spender'
+        WHEN purchase_amount >= 50 THEN 'Medium Spender'
+        ELSE 'Low Spender'
+    END AS spending_level
+FROM customer_purchases;
+
+SELECT
+    CASE
+        WHEN purchase_amount >= 80 THEN 'High Spender'
+        WHEN purchase_amount >= 50 THEN 'Medium Spender'
+        ELSE 'Low Spender'
+    END AS spending_level,
+    COUNT(*) AS total_customers
+FROM customer_purchases
+GROUP BY spending_level;
+
+-- Row Number by Purchase Amount
+SELECT
+    customer_id,
+    purchase_amount,
+    ROW_NUMBER() OVER (ORDER BY purchase_amount DESC) AS row_num
+FROM customer_purchases;
+
+-- ==========================================
+-- Query 43 : Dense Rank
+-- ==========================================
+
+SELECT
+    customer_id,
+    purchase_amount,
+    DENSE_RANK() OVER (ORDER BY purchase_amount DESC) AS dense_ranks
+FROM customer_purchases;
+
+SELECT
+    customer_id,
+    purchase_amount,
+    RANK() OVER (ORDER BY purchase_amount DESC) AS customer_rank
+FROM customer_purchases;
  
